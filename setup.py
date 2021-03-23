f1b5f4d3bf7e57b528123fbf96faf7283ef6410b
Gustavo Suto <guyrux@gmail.com>
2021-03-23 08:35:23 -0300

    :tada: First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Simple regression problem to practice oop development in machine learning.',
+    author='Suto',
+    license='BSD-3',
+)
